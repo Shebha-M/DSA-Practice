feded9685367bb473010e2c2aad0b8d76044129f
Shebha-M <shebham.06@gmail.com>
2026-05-24 15:13:59 +0530

    Solved maximum subarray sum using Kadane algorithm

---
+++ Gfg/Arrays/kadanes_algorithm.cpp
@@ -0,0 +1,24 @@
+// Problem: Maximum Subarray Sum
+// Platform: GeeksforGeeks
+// Pattern: Kadane's Algorithm
+
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) 
+    {
+        int n = arr.size();
+        int maxi = INT_MIN;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+
+            maxi = max(maxi, sum);
+
+            if(sum < 0)
+                sum = 0;
+        }
+        return maxi;
+    }
+};
